e1d3cc9d988efe71437f580ddb10367b6f53310b
hafisardiyanto <hafisardiyanto19@gmail.com>
2026-07-21 22:21:14 +0700

    menghilangka a href url Email

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
     <h2>Kontak</h2>
     <p>Silakan hubungi saya melalui:</p>
     <ul class="contact-list">
-      <li><strong>Email:</strong> <a href="mailto:hafisardiyanto19@gmail.com">hafisardiyanto19@gmail.com</a></li>
+      <li><strong>Email:</strong> hafisardiyanto19@gmail.com </li>
       <li><strong>WhatsApp:</strong> <a href="https://wa.me/6285748174062" target="_blank" rel="noopener noreferrer">085748174062</a></li>
       <li><strong>GitHub:</strong> <a href="https://github.com/hafisardiyanto" target="_blank" rel="noopener noreferrer">https://github.com/hafisardiyanto</a></li>
       <li><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/hafis-ardiyanto-805b65254/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/hafis-ardiyanto-805b65254/</a></li>
